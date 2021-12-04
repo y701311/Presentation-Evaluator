e689a2aa9084928a14ee4a5f984fca6ee929516e
sato1701 <a3city17@gmail.com>
2021-12-04 12:26:28 +0900

    ADD: Made Help Class
    ・openHelpメゾットからdisplayHelpメゾット、setHelpTextメゾットに分割しました。
    ・displayHelpメゾット、setHelpTextメゾットをそれぞれ実装しました。
    ・コンストラクタをpublicで新規作成しました。

--- app/src/main/java/com/presenter/Help.java
+++ app/src/main/java/com/presenter/Help.java
@@ -1,16 +1,48 @@
 package com.presenter;
 
-import java.nio.file.Path;
+import android.content.res.AssetManager;
 
-public class Help{
+import com.main.MainActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Help {
+    private final MainActivity activity;
+    private final com.view.HelpOpen helpOpen;
     private String helpText;
-    private String helpText(){
-        return null;
+
+    public Help(MainActivity activity) {
+        this.activity = activity;
+        helpText = "";
+        helpOpen = new com.view.HelpOpen();
+        setHelpText();
     }
-    public void helpClose(){
+
+    public void helpClose() {
 
     }
-    private String openHelpText(){
-        return null;
+
+    public void displayHelp() {
+        helpOpen.displayHelpText(helpText);
+    }
+
+    private void setHelpText() {
+        InputStream stream;
+        BufferedReader bufferedReader;
+        AssetManager assetManager;
+        try {
+            assetManager = activity.getResources().getAssets();
+            stream = assetManager.open("HelpText/HelpText.txt");
+            bufferedReader = new BufferedReader(new InputStreamReader(stream));
+            while (bufferedReader.ready()) {
+                helpText = helpText.concat(bufferedReader.readLine());
+            }
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
