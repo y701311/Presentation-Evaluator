ea7c2fabc36591c7e7b3c1c9a422e7f1e6fe177d
sato1701 <a3city17@gmail.com>
2021-12-06 15:38:57 +0900

    ADD: Made Evaluator Class
    ・コンストラクタをpublicで新規作成しました。
    ・startEvaluateを実装しました。
    ・setEvaluationValuesを削除しました。
    ・getEvaluationValuesを新規作成しました。
    ・ResultDisplayクラスにて、resultDisplayメゾットをdisplayに改名しました。

--- app/src/main/java/com/presenter/Evaluator.java
+++ app/src/main/java/com/presenter/Evaluator.java
@@ -1,13 +1,31 @@
 package com.presenter;
 
+import android.content.Context;
+
+import com.main.MainActivity;
+import com.model.EvalController;
+
 import java.nio.file.Path;
 
-public class Evaluator{
+public class Evaluator {
+    private final MainActivity activity;
     private EvaluationValues value;
-    public void startEvaluate(/*TODO Path audioFilePath*/){
 
+    public Evaluator(MainActivity activity) {
+        this.activity = activity;
     }
-    private void setEvaluationValues(){
 
+    public void startEvaluate() {
+        ResultDisplay resultDisplay = new ResultDisplay();
+        FileSelect fileSelect = new FileSelect();
+        EvalController evalController = new EvalController();
+        Path audioFilePath = fileSelect.getFilePath();
+
+        value = evalController.evalController(audioFilePath, (Context) activity);
+        resultDisplay.display(value);
+    }
+
+    EvaluationValues getEvaluationValues() {
+        return value;
     }
 }
