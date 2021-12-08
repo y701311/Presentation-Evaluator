cdcd215fb9a283425de6f399202186e988f3e5b2
y70131 <yuukiyamada2005@gmail.com>
2021-12-08 23:21:34 +0900

    VolumeEvalクラスの実装

--- app/src/main/java/com/model/VolumeEval.java
+++ app/src/main/java/com/model/VolumeEval.java
@@ -1,15 +1,39 @@
 package com.model;
 
+import java.util.ArrayList;
+
 class VolumeEval extends Evaluator {
-    private double[] evalValue;
+    private ArrayList<Double> evalValue;
 
+    VolumeEval() {
+        this.evalValue = new ArrayList<Double>();
+    }
+
+    // audioDataの実効値のデシベルを計算する
     @Override
-    void calculation(/* TODO audioData*/) {
+    void calculation(double[] audioData) {
+        double evalValue;
+        evalValue = getDecibel(Utility.getRms(audioData));
 
+        this.evalValue.add(evalValue);
+    }
+
+    // 人間の最小可聴音圧である20[μPa]を基準としたdBを返す
+    double getDecibel(double rms) {
+        final double Base = 0.00002;
+        return 20 * Math.log10(rms / Base);
     }
 
     @Override
     double returnResult() {
-        return 0.0;
+        double evalValue = 0;
+
+        // デシベルの平均値を算出
+        for(double value : this.evalValue){
+            evalValue += value;
+        }
+        evalValue /= this.evalValue.size();
+
+        return evalValue;
     }
 }
