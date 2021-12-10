671d43b7a978b95f78f4eb92b8e6bddda099b005
y70131 <yuukiyamada2005@gmail.com>
2021-12-10 20:12:26 +0900

    AccentEvalクラスの実装

--- app/src/main/java/com/model/AccentsEval.java
+++ app/src/main/java/com/model/AccentsEval.java
@@ -9,13 +9,41 @@ class AccentsEval extends Evaluator {
         this.evalValue = new ArrayList<Double>();
     }
 
+    // audioDataの実効値のデシベルを計算する
     @Override
     void calculation(double[] audioData) {
+        double evalValue;
+        final double speakingDecibel = 30;
 
+        evalValue = Utility.getDecibel(Utility.getRms(audioData));
+
+        // 話していると判定したデータのみについて評価
+        if(evalValue >= speakingDecibel) {
+            this.evalValue.add(evalValue);
+        }
     }
 
     @Override
     double returnResult() {
-        return 0.0;
+        double score;
+        final double diffRateMin = 3, diffRateMax = 10;
+        double diffSum = 0, diffRate;
+
+        // データと、その1つ前のデータとの差の平均値を算出
+        for(int i = 1; i < this.evalValue.size(); i++){
+            diffSum += Math.abs(this.evalValue.get(i) - this.evalValue.get(i-1));
+        }
+        diffRate = diffSum / this.evalValue.size();
+
+        // 点数化
+        if(diffRate >= diffRateMax){
+            score = 100;
+        }else if(diffRate <= diffRateMin){
+            score = 0;
+        }else{
+            score = 100 * (diffRate - diffRateMin) / (diffRateMax - diffRateMin);
+        }
+
+        return score;
     }
 }
