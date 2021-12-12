a069c5ea2a8e3d995101e43bcbde9b9af81adefc
sato1701 <a3city17@gmail.com>
2021-12-12 13:55:37 +0900

    ADD: EvaluationValuesの変数を追加
    ・評価の参考となる、各評価値のコメント入れる変数を追加しました。
    ・評価の総合点数となるtotalScoreを追加しました。

--- app/src/main/java/com/presenter/EvaluationValues.java
+++ app/src/main/java/com/presenter/EvaluationValues.java
@@ -2,8 +2,15 @@ package com.presenter;
 
 public class EvaluationValues{
     public double speakingInterval;
+    public String speakingIntervalText;
     public double volume;
+    public String volumeText;
     public double speakingSpeed;
+    public String speakingSpeedText;
     public double meanLessWords;
+    public String meanLessWordsText;
     public double accents;
+    public String accentsText;
+    public double total;
+    public String totalText;
 }
