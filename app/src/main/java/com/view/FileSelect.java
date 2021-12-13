1a6efb1a66eb2e5ab8070daeef6446fab4e53e98
ouu-0000 <ouu.jj833@icloud.com>
2021-12-13 20:31:41 +0900

    12/13の進捗

--- app/src/main/java/com/view/FileSelect.java
+++ app/src/main/java/com/view/FileSelect.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.main.R;
 
 public class FileSelect extends AppCompatActivity {
