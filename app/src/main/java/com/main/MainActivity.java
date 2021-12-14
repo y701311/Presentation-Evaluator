c468b72324e411bb7e3d235ece9f262b9df51723
LogGC-num <79786466+LogGC-num@users.noreply.github.com>
2021-12-14 17:19:04 +0900

    これならIntentを呼び出してファイルパスを受け取ることができる

--- app/src/main/java/com/main/MainActivity.java
+++ app/src/main/java/com/main/MainActivity.java
@@ -9,19 +9,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.util.Log;
 
 import com.model.FileSelect;
 
 public class MainActivity extends AppCompatActivity {
-    FileSelect fs = new FileSelect(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("banana");
-
-        fs.searchFilePath();
+        openFile(null);
     }
+
+    ActivityResultLauncher<Intent> _launcherSelectAudioFile =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent resultData = result.getData();
+                        if (resultData != null) {
+                            Uri uri = resultData.getData();
+                            Log.d("Result File Path", "" + uri); // 取得してきたパス表示
+                            if (uri != null) {
+                                // to do something（URI取得できた時にやりたい処理をここに記述）
+                                //editText.setText(loadStrFromUri(uri));
+                                System.out.println("\nResult File Path\n" + uri);
+                            }
+                        }
+                    }
+                }
+            });
+    private void openFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("audio/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        Intent chooserIntent = Intent.createChooser(intent, "音声ファイルの選択");
+        _launcherSelectAudioFile.launch(chooserIntent);
+
+    }
+
 }
--- app/src/main/java/com/model/FileSelect.java
+++ app/src/main/java/com/model/FileSelect.java
@@ -19,54 +19,15 @@ import com.main.MainActivity;
 import java.nio.file.Path;
 import java.util.Objects;
 
-public class FileSelect extends AppCompatActivity {
+public class FileSelect extends MainActivity {
 
-    private AppCompatActivity appActivity;
-    public FileSelect(AppCompatActivity mainActivity){ appActivity = mainActivity ;};
+//    private AppCompatActivity appActivity;
+//    public FileSelect(AppCompatActivity mainActivity){ appActivity = mainActivity ;};
 
     Uri ReturnUri = null;
     public Uri searchFilePath(){
-        System.out.println("before");
-        openFile(null);
+        //openfile();
         return ReturnUri;
-    }
-
-    ActivityResultLauncher<Intent> _launcherSelectAudioFile =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-        @Override
-        public void onActivityResult(ActivityResult result) {
-            if (result.getResultCode() == RESULT_OK) {
-                Intent resultData = result.getData();
-                if (resultData != null) {
-                    Uri uri = resultData.getData();
-                    Log.d("Result File Path", "" + uri); // 取得してきたパス表示
-                    if (uri != null) {
-                        // to do something（URI取得できた時にやりたい処理をここに記述）
-                        //editText.setText(loadStrFromUri(uri));
-                        System.out.println("\nResult File Path\n" + uri);
-                    }
-                }
-            }
-        }
-    });
-    private void openFile(Uri pickerInitialUri) {
-        if(appActivity==null){
-            System.out.println("nullpo");
-        }
-        else{
-            System.out.println("Yeah!!");;
-        }
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("audio/*");
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-        Intent chooserIntent = Intent.createChooser(intent, "音声ファイルの選択");
-        System.out.println("?????");
-        if(chooserIntent == null) {
-            System.out.println("null");
-        }
-        _launcherSelectAudioFile.launch(chooserIntent);
-
     }
 
 
