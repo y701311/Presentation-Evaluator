830dab721c6bce97b733a2b03d86a1bceec191e4
sato1701 <a3city17@gmail.com>
2021-12-18 05:36:32 +0900

    FIX: accentsの評価係数等の調整
    ・evalResultの評価式を変更

--- app/src/main/java/com/model/AccentsEval.java
+++ app/src/main/java/com/model/AccentsEval.java
@@ -3,7 +3,7 @@ package com.model;
 import java.util.ArrayList;
 
 class AccentsEval extends Evaluator {
-    private ArrayList<Double> evalValue;
+    private final ArrayList<Double> evalValue;
 
     AccentsEval() {
         this.evalValue = new ArrayList<Double>();
@@ -26,7 +26,7 @@ class AccentsEval extends Evaluator {
     @Override
     EvalResult returnResult() {
         EvalResult evalResult = new EvalResult();
-        final double bestDiffRate = 2.5;
+        final double bestDiffRate = 4.8;
         double diffSum = 0, diffRate;
 
         // データと、その1つ前のデータとの差の平均値を算出
@@ -40,19 +40,19 @@ class AccentsEval extends Evaluator {
         }
 
         // 点数化
-        evalResult.score = 100 - 100 * Math.abs(diffRate - bestDiffRate) / bestDiffRate;
+        evalResult.score = 100 - 50 * Math.abs(diffRate - bestDiffRate);
         if(evalResult.score < 0){
             evalResult.score = 0;
         }
 
         if(0 <= evalResult.score && evalResult.score < 20){
-            evalResult.text = "メリハリを意識してみよう";
-        }else if(20 <= evalResult.score && evalResult.score < 40){
             evalResult.text = "大事な言葉を意識しよう";
+        }else if(20 <= evalResult.score && evalResult.score < 40){
+            evalResult.text = "メリハリを意識してみよう";
         }else if(40 <= evalResult.score && evalResult.score < 60){
-            evalResult.text = "表現を工夫してみよう";
+            evalResult.text = "正しい強弱を意識してみよう";
         }else if(60 <= evalResult.score && evalResult.score < 80){
-            evalResult.text = "正しい抑揚を意識してみよう";
+            evalResult.text = "表現を工夫してみよう";
         }else if(80 <= evalResult.score && evalResult.score <= 100){
             evalResult.text = "完璧！";
         }
